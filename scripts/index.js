8bed8ec68ba0f71441f8d7ab8a6e6fb2f1ad4f5e
Veronika <nika.zhevner1307@icloud.com>
2021-02-24 11:31:01 +0200

    fix: Index js; html; delete min-height

--- scripts/index.js
+++ scripts/index.js
@@ -10,15 +10,15 @@ let profile = document.querySelector('.profile');
 let profileName = profile.querySelector('.profile__name');
 let profileCaption = profile.querySelector('.profile__caption');
 
-let likeButtons = document.querySelectorAll('.element__like-button');
-
 init();
 
 function togglePopup() {
     let popUpStatus = 'popup_status_opened';
-
-    nameInput.value = profileName.innerText;
-    jobInput.value = profileCaption.innerText;
+    
+    if(!popUp.classList.contains(popUpStatus)){
+        nameInput.value = profileName.innerText;
+        jobInput.value = profileCaption.innerText;
+    }
 
     popUp.classList.toggle(popUpStatus);
 }
@@ -35,16 +35,10 @@ function updateProfile(name, caption) {
     profileCaption.innerText = caption;
 }
 
-function likeButtonActive(evt) {
-    let likeButtonStatus = 'element__like-button_status_active';
-    evt.srcElement.classList.toggle(likeButtonStatus);
-}
-
 function init() {
     updateProfile('Жак-Ив Кусто', 'Исследователь океана');
 
     openPopupButton.addEventListener('click', togglePopup);
     closePopupButton.addEventListener('click', togglePopup);
     formElement.addEventListener('submit', formSubmitHandler);
-    likeButtons.forEach(item => item.addEventListener('click', likeButtonActive));
 }
